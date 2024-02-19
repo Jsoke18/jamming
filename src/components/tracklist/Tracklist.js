9b54a995cc3587f8f9221ee7af56818c6015b062
Josh Soke <josh@fitzba.com>
2024-02-19 12:41:07 -0600

    fixed URI

--- src/components/tracklist/Tracklist.js
+++ src/components/tracklist/Tracklist.js
@@ -2,9 +2,18 @@ import { Box } from "@chakra-ui/react"; // Import Box from Chakra UI
 import { Track } from "../track/track"; // Import the Track component
 
 export const TrackList = ({ tracks, onAdd, onRemove, context }) => {
-  return tracks.map(track => (
-    <Box marginBottom={4} key={track.id}> 
-      <Track track={track} onAdd={onAdd} onRemove={onRemove} context={context} />
+  return (
+    <Box>
+      {tracks.map((track, index) => (
+        // Using a combination of track id and index to ensure uniqueness
+        <Track 
+          key={`${track.id}-${index}`} 
+          track={track} 
+          onAdd={onAdd} 
+          onRemove={onRemove} 
+          context={context} 
+        />
+      ))}
     </Box>
-  ));
+  );
 };
